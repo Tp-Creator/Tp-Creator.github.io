afdcff941972846ef6552bacbfdafd340782cc64
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-05-24 17:44:07 +0200

    hej

--- javascript/general.js
+++ javascript/general.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var navbar = document.getElementById('navbar');
     
     window.addEventListener('scroll', function() {
-        if (window.scrollY < window.innerHeight*0.9) {
+        if (window.scrollY < window.innerHeight) {
             navbar.style.backgroundColor = 'transparent';
         } else {
             navbar.style.backgroundColor = 'var(--background)';
